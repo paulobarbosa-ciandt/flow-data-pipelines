c9776f35ee8e3813413dd3f8aeb5a8deb81528da
Paulo Barbosa <paulob@ciandt.com>
2024-11-20 11:31:23 +0000

    added sources

--- flow_data_pipeline/models/bronze/member_data.sql
+++ flow_data_pipeline/models/bronze/member_data.sql
@@ -21,3 +21,51 @@ with source_data as (
 )
 
 select * from source_data
+
+{{ config(
+    materialized='incremental',
+    unique_key='member_id',
+    incremental_strategy='merge',
+    on_schema_change='sync_all_columns'
+) }}
+
+WITH source_data AS (
+    SELECT
+        member_id,
+        first_name,
+        last_name,
+        date_of_birth,
+        gender,
+        address,
+        phone_number,
+        email,
+        join_date,
+        employer_id,
+        modified_date,
+        current_timestamp() AS create_date,
+        current_timestamp() AS update_date
+    FROM
+        '{{ source('azure_blob_storage', 'raw_members') }}'
+)
+
+SELECT
+    member_id,
+    first_name,
+    last_name,
+    date_of_birth,
+    gender,
+    address,
+    phone_number,
+    email,
+    join_date,
+    employer_id,
+    modified_date,
+    create_date,
+    update_date
+FROM
+    source_data
+
+{% if is_incremental() %}
+WHERE
+    modified_date > (SELECT MAX(modified_date) FROM {{ this }})
+{% endif %}
