5acc18ee8e6923a09b3cbffde013bf6e6af9056e
Paulo Barbosa <paulob@ciandt.com>
2024-11-21 00:52:02 +0000

    adjusting model

--- flow_data_pipeline/models/bronze/member_data.sql
+++ flow_data_pipeline/models/bronze/member_data.sql
@@ -21,7 +21,7 @@ WITH source_data AS (
         current_timestamp() AS create_date,
         current_timestamp() AS update_date
     FROM
-        '{{ source('fivetran', 'raw_members') }}'
+        {{ source('fivetran', 'raw_members') }}
 )
 
 SELECT
