8e3e5530f54086ae311626f2b2910db6a633b4dd
Paulo Barbosa <paulob@ciandt.com>
2024-11-21 00:41:56 +0000

    adjusting member model

--- flow_data_pipeline/models/bronze/member_data.sql
+++ flow_data_pipeline/models/bronze/member_data.sql
@@ -1,30 +1,6 @@
 {{ config(
     materialized='incremental',
     unique_key='member_id',
-    on_schema_change='sync_all_columns'
-) }}
-
-with source_data as (
-    select
-        member_id,
-        first_name,
-        last_name,
-        date_of_birth,
-        contribution_type,
-        withdrawal_reason,
-        units_held,
-        unit_price,
-        current_timestamp() as create_date,
-        current_timestamp() as update_date
-    from
-        parquet.`/mnt/flowdataefficiencydemo/fivetran/raw_members/*.parquet`
-)
-
-select * from source_data
-
-{{ config(
-    materialized='incremental',
-    unique_key='member_id',
     incremental_strategy='merge',
     on_schema_change='sync_all_columns'
 ) }}
